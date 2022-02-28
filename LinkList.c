7debd770aee30222d2ddcc84f2513091814f7ff7
lgr1001lzq <15730053483@139.com>
2022-02-28 09:27:27 +0800

    master

--- LinkList.c
+++ LinkList.c
@@ -4,7 +4,7 @@ typedef struct Node{
     int data;
     struct Node *next;
 }Node;
-void createLinkList(Node *head,int size)
+void CreateLinkList(Node *head,int size)
 {
     Node *rear=head;
     int i;
@@ -26,4 +26,71 @@ void TreaveLinkList(Node *head)
         p=p->next;
     }
     putchar('\n');
+}
+void InsertForword(Node *head,int data){
+    Node *newnode=(Node*)malloc(sizeof(Node));
+    newnode->next=NULL;
+    newnode->data=data;
+    newnode->next=head->next;
+    head->next=newnode;
+}
+void DeleteSame(Node *head){
+    Node *curr=head->next;
+    while(curr!=NULL){
+        Node *pre=curr;
+        Node *p=curr->next;
+        while(p!=NULL){
+            if(curr->data==p->data){
+                pre->next=p->next;
+                free(p);
+                p=pre->next;
+            }
+            else{
+                pre=pre->next;
+                p=p->next;
+            }
+        }
+        curr=curr->next;
+    }
+}
+void ReverseLinklist(Node *head){
+    Node *curr;
+    Node *pre=NULL;
+    while(head->next!=NULL){
+        curr=head->next;
+        head->next=curr->next;
+        curr->next=pre;
+        pre=curr;        
+    }
+    head->next=pre;
+}
+void DeleteLinklist(Node *head){
+    Node *curr;
+    while(head->next!=NULL){
+        curr=head->next;
+        head->next=curr->next;
+        free(curr);
+    }
+}
+void Isempty(Node *head){
+    if(head->next!=NULL){
+        printf("该链表不为空！\n");
+    }
+    else{
+        printf("该链表为空！\n");
+    }
+}
+void main()
+{
+    Node *head=(Node *)malloc(sizeof(Node));
+    head->next=NULL;
+    CreateLinkList(head,15);
+    Isempty(head);
+    TreaveLinkList(head);
+    InsertForword(head,20);
+    TreaveLinkList(head);
+    DeleteSame(head);
+    TreaveLinkList(head);
+    DeleteLinklist(head);
+    Isempty(head);
 }
